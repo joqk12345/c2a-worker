383b2572980c3382e205ce93db2ae0ee695b3de6
kai.qiao <kai.qiao@shopee.com>
2023-11-23 09:59:28 +0800

    控制线程+worker线程

--- handler/worker.py
+++ handler/worker.py
@@ -6,12 +6,17 @@ from apis.c2a import state
 
 def process_worker():
     while True:
-        logger.notice("running state:{}",state())
-        logger.notice("processing")
-        time.sleep(5)
+        logger.notice("running state:{}", state())
+        runnning_state = state()['state']
+        if(runnning_state == "running"):
+            logger.notice("processing")
+            time.sleep(5)
+        else:
+            logger.swarn(5,"current running state :{},wait for a moment",runnning_state)
+
 
 def entrypoint():
-    worker_num = 1
+    worker_num = 4
     workers = []
     for idx in range(worker_num):
         logger.notice("start: {}", idx)
